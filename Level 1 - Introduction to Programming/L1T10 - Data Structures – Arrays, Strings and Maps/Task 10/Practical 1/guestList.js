470dab955778af0ef65c49f7a01e8df6dbcc9158
Raquel-Andrea <raqueld323@gmail.com>
2026-07-21 17:18:26 +0200

    Add files via upload

--- Level 1 - Introduction to Programming/L1T10 - Data Structures – Arrays, Strings and Maps/Task 10/Practical 1/guestList.js
+++ Level 1 - Introduction to Programming/L1T10 - Data Structures – Arrays, Strings and Maps/Task 10/Practical 1/guestList.js
@@ -1,22 +1,34 @@
+// Create an empty array to store the guest names
 let guestList = [];
+// Loop to prompt the user for guest names
 for(let i = 1; i <= 11; i++){
 
+    // Prompt the user to enter a guest name
 let name = prompt("Enter guest " + i + "'s name:");
+
+// Check if the guest list has less than 10 names
 if(guestList.length < 10) {
     guestList.push(name);
 }
 else{
+    // If the guest list already has 10 names, ask if they want to replace someone
     let answer = prompt(
-        "You can only add a maximum of 10 people./nWould you like to replace someone with " + name + "? (yes/no)" 
+        "You can only add a maximum of 10 people.\nWould you like to replace someone with " + name + "? (yes/no)" 
     );
+    //
 answer = answer.toLowerCase();
-if(anwser == "yes") {
-let replaceName = prompt("Enter the name of the guest you want to replace:");
-let index = guestList.indexOf(replaceName);
+//
+if(answer == "yes") {
+// Prompt the user to enter the name of the guest they want to replace
+    let replaceName = prompt("Enter the name of the guest you want to replace:");
+//
+    let index = guestList.indexOf(replaceName);
+    //
 if (index != -1) {
     guestList[index] = name;
 }
 }
 }
 }
+//
 alert("Guest List:\n" + guestList.join(","));
--- Level 1 - Introduction to Programming/L1T10 - Data Structures – Arrays, Strings and Maps/Task 10/Practical 2/translator.js
+++ Level 1 - Introduction to Programming/L1T10 - Data Structures – Arrays, Strings and Maps/Task 10/Practical 2/translator.js
@@ -1,5 +1,7 @@
+// Create a Map to store English words and their Afrikaans translations
 let translator = new Map();
 
+// Add English and Afrikaans word pairs to the dictionary
 translator.set("hello", "hallo");
 translator.set("goodbye", "totsiens");
 translator.set("dog", "hond");
@@ -11,10 +13,17 @@ translator.set("school", "skool");
 translator.set("water", "water");
 translator.set("food", "kos");
 
+//Ask the user to enter an English word
 let word = prompt(" Enter an English word to translate:");
+
+//Convert the user's input to lowercase so the search is not case-sensitive
 word = word.toLowerCase();
+
+//Check whether the word exists in the dictionary
 if (translator.has(word)) {
+    //Display the Afrikaans translation
     alert("Translation: " +translator.get(word));}
 else{
+    //Inform the user if the word cannot be found
     alert("Sorry, that word is not in the dictionary.");
 }
